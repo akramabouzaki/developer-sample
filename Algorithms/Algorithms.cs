e60d5b73a21c4b2c03d909885f5e489cfd20d931
akramabouzaki <akramabouzaki@hotmail.com>
2023-11-03 12:38:24 -0400

    Update Algorithms.cs

--- Algorithms/Algorithms.cs
+++ Algorithms/Algorithms.cs
@@ -1,11 +1,29 @@
 using System;
+using System.Linq;
 
 namespace DeveloperSample.Algorithms
 {
     public static class Algorithms
     {
-        public static int GetFactorial(int n) => throw new NotImplementedException();
+        public static int GetFactorial(int n) {
+            if (n <= 1)
+                return 1;
+            else
+                return (n * GetFactorial(n - 1));
+        }
 
-        public static string FormatSeparators(params string[] items) => throw new NotImplementedException();
+        public static string FormatSeparators(params string[] items)
+        {
+            if (items.Length == 1)
+                return items[0];
+            else
+            {
+                string[] temp = new string[items.Length - 1];
+                Array.Copy(items, temp, items.Length - 1);
+                string seperatedByComma = String.Join(", ", temp);
+                string result = seperatedByComma + " and " + items[items.Length - 1];
+                return result;
+            }
+        }
     }
 }
