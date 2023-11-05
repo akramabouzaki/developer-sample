b1e0985ed210f9349f0d6c900870a94506cddb64
akramabouzaki <akramabouzaki@hotmail.com>
2023-11-05 12:37:32 -0500

    Update Container.cs
    My solution

--- Container/Container.cs
+++ Container/Container.cs
@@ -1,10 +1,34 @@
 using System;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace DeveloperSample.Container
 {
     public class Container
     {
-        public void Bind(Type interfaceType, Type implementationType) => throw new NotImplementedException();
-        public T Get<T>() => throw new NotImplementedException();
+        private Dictionary<Type, Type> bindingList = new Dictionary<Type, Type>();
+        public void Bind(Type interfaceType, Type implementationType)
+        {
+            bool isValidParams = (interfaceType != null) && (implementationType != null) &&
+                                interfaceType.IsInterface && implementationType.IsClass;
+            if (!isValidParams)
+                throw new Exception("Args invalid!");
+            bool isInterfaceImplemnted = implementationType.GetInterface(interfaceType.Name) == interfaceType;
+            if (!isInterfaceImplemnted)
+                throw new Exception("Interface is not inherited by class!");
+            bindingList.Add(interfaceType, implementationType);
+        }
+        public T Get<T>()
+        {
+            Type t = typeof(T);
+            if (bindingList.ContainsKey(t))
+                return (T)Activator.CreateInstance(bindingList[t]);
+            else if (t.IsClass)
+                return (T)Activator.CreateInstance(t);
+            else if (t.IsInterface)
+                throw new Exception("Interface can't be initialized without binding to class first!");
+            else
+                throw new Exception("Type T should be a class or interface!");
+        }
     }
 }
