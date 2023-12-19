7ea868f6c74726dae7767f25a031389cdde2d090
akramabouzaki <akramabouzaki@hotmail.com>
2023-12-18 19:24:56 -0500

    Improve readability of Container.cs
    Improve code readability

--- Container/Container.cs
+++ Container/Container.cs
@@ -9,26 +9,43 @@ namespace DeveloperSample.Container
         private Dictionary<Type, Type> bindingList = new Dictionary<Type, Type>();
         public void Bind(Type interfaceType, Type implementationType)
         {
-            bool isValidParams = (interfaceType != null) && (implementationType != null) &&
-                                interfaceType.IsInterface && implementationType.IsClass;
+            bool isValidParams = (interfaceType != null) &&
+                                 (implementationType != null) &&
+                                 interfaceType.IsInterface && 
+                                 implementationType.IsClass;
             if (!isValidParams)
+            {
                 throw new Exception("Args invalid!");
+            }
+            
             bool isInterfaceImplemnted = implementationType.GetInterface(interfaceType.Name) == interfaceType;
+            
             if (!isInterfaceImplemnted)
+            {
                 throw new Exception("Interface is not inherited by class!");
+            }
+            
             bindingList.Add(interfaceType, implementationType);
         }
         public T Get<T>()
         {
             Type t = typeof(T);
             if (bindingList.ContainsKey(t))
+            {
                 return (T)Activator.CreateInstance(bindingList[t]);
+            }
             else if (t.IsClass)
+            {
                 return (T)Activator.CreateInstance(t);
+            }
             else if (t.IsInterface)
-                throw new Exception("Interface can't be initialized without binding to class first!");
+            {
+                throw new Exception("Interface can not be initialized without binding to class first!");
+            }
             else
-                throw new Exception("Type T should be a class or interface!");
+            {
+                throw new Exception("Type T should be a class or an interface!");
+            }
         }
     }
 }
